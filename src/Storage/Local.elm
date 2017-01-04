971877cfc8121c03650eb405f117b019a5da0bb5
KatsCyl <ahti.katiska@relex.fi>
2017-01-04 08:18:02 +0200

    Upgrade to Elm 0.18

--- src/Storage/Local.elm
+++ src/Storage/Local.elm
@@ -1,38 +1,48 @@
-module Storage.Local exposing (..) 
+module Storage.Local exposing (..)
+
+{-| This is a library providing simple binding to LocalStorage
+
+# Cache object
+@docs object
+
+# Functions
+@docs put, get, isAvailable
+
+-}
 
 import Native.Storage.Local
 import Json.Decode as Json
 
-
+{-| Straight bind to LocalStorage.put -}
 put : String -> Json.Value -> Result String ()
 put =
-  Native.Storage.Local.put
+    Native.Storage.Local.put
 
-
+{-| Straight bind to LocalStorage.get -}
 get : String -> Result String Json.Value
 get =
-  Native.Storage.Local.get
+    Native.Storage.Local.get
 
-
+{-| Straight bind to LocalStorage.isAvailable -}
 isAvailable : Bool
 isAvailable =
-  Native.Storage.Local.isAvailable
+    Native.Storage.Local.isAvailable
 
-
+{-| Create a record with necessary put and get functions for data -}
 object :
-  String
-  -> (a -> Json.Value)
-  -> Json.Decoder a
-  -> { put : a -> Result String a
-     , get : () -> Result String a
-     }
+    String
+    -> (a -> Json.Value)
+    -> Json.Decoder a
+    -> { put : a -> Result String a
+       , get : () -> Result String a
+       }
 object key encode decoder =
-  { put =
-      \value ->
-        put key (encode value)
-          |> Result.map (always value)
-  , get =
-      \_ ->
-        get key
-          `Result.andThen` (Json.decodeValue decoder)
-  }
+    { put =
+        \value ->
+            put key (encode value)
+                |> Result.map (always value)
+    , get =
+        \_ ->
+            get key
+                |> Result.andThen (Json.decodeValue decoder)
+    }
